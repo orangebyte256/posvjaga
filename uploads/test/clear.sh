dcd83ea986a1fb466413d7e01d9b6f90fa7af7da
orangebyte256 <orangebyte256@gmail.com>
2015-11-16 18:55:37 +0600

    theird

---
+++ uploads/test/clear.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd $2
+rm "$1_source.c"
+rm $1
+rm "$1_temp.txt"
---
+++ uploads/test/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd $2
+gcc "$1_source.c" -o $1 2> /dev/null
+if [ ! $? -eq 0 ]; then
+    echo "compile error"
+    exit
+fi
+for i in {0..4}
+do
+	name="test_$i.txt"
+	answer="answer_$i.txt"
+	temp="$1_temp.txt"
+	echo "" > $temp;
+	timeout 1 ./$1 < $name > $temp;
+	if ! [ -s $temp ]
+	then
+		echo "time limit on $i test"
+		exit
+	else
+		if (! cmp $temp $answer > /dev/null) then
+			echo "validation error on $i test"
+			exit
+		fi
+	fi
+done
+echo "success"
